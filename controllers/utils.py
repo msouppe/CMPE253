b1942bf54e1f828ee598e6333f3d4c8031ef99be
Aasim Khan <aasimkhan30@gmail.com>
2019-03-20 18:25:16 -0700

    BFT commit works with leader election and state transfer

--- controllers/utils.py
+++ controllers/utils.py
@@ -11,43 +11,44 @@ def btf_commit(server_id, nib_path, nib):
     subprocess.Popen(["java", "-jar", nib_path+"sdnstore.jar", "-client", str(server_id+4), str(1), nib_path+"nib.txt"])
 
 def leader_election(server_id, jar_path):
-    subprocess.Popen(["java", "-jar", "./sdnstore.jar", "-client", str(server_id+4), str(0), str(server_id), jar_path])
+  p = subprocess.Popen(["java", "-jar", "./sdnstore.jar", "-client", str(server_id+4), str(0), str(server_id), jar_path])
+    
     
 def start_server(server_id, absolute_path):
-    print("java -jar "+absolute_path+"sdnstore.jar -server "+str(server_id)+" "+absolute_path+"/nib.txt &")
+    print("java -jar "+absolute_path+"sdnstore.jar -server "+str(server_id)+" "+absolute_path+"nib.txt &")
     subprocess.Popen(["java", "-jar", "./sdnstore.jar", "-server", str(server_id), absolute_path+"nib.txt", "&"])
     #os.spawnl(os.P_NOWAIT, "java -jar "+absolute_path+"sdnstore.jar -server "+str(server_id)+" "+absolute_path+"/nib.txt &")
-    print("Hello")
-
+def simple():
+  print("Hello")
 nib = {
     "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-start_server(0, "./c0/")
+start_server(0, "./")
 time.sleep(2)
-start_server(1, "./c1/")
+start_server(1, "./")
 time.sleep(2)
-start_server(2, "./c2/")
+start_server(2, "./")
 time.sleep(2)
-start_server(3, "./c3/")
+start_server(3, "./")
 time.sleep(5)
 leader_election(0,"./c0")
 time.sleep(2)
-leader_election(1,"./c1")
-time.sleep(2)
-leader_election(2,"./c2")
-time.sleep(2)
-leader_election(3,"./c3")
-time.sleep(310)
-leader_election(3,"./c3")
-# btf_commit(0,"./c0/",nib)
-# time.sleep(10)
-# with open('./c1/nib.txt', 'rb') as handle:
-#     b = pickle.load(handle)
-# if(nib==b):
-#     print("Success =======================")
-# else:
-#     print("Failure=========================")
+# leader_election(1,"./c1")
+# time.sleep(2)
+# leader_election(2,"./c2")
+# time.sleep(2)
+# leader_election(3,"./c3")
+# time.sleep(310)
+# leader_election(3,"./c4")
+# # btf_commit(0,"./c0/",nib)
+# # time.sleep(10)
+# # with open('./c1/nib.txt', 'rb') as handle:
+# #     b = pickle.load(handle)
+# # if(nib==b):
+# #     print("Success =======================")
+# # else:
+# #     print("Failure=========================")
 
-subprocess.Popen(["pkill", "-f", "'java -jar'"])
+# subprocess.Popen(["pkill", "-f", "'java -jar'"])
---
+++ run-allcon.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#! /bin/bash
+
+# Kill Java process
+
+# Determine the pid
+PID=`ps -C java -o pid=`
+
+kill -9 $PID
+
+kill $(ps aux | grep controller | awk '{print $2}') 
+
+controller0="/home/parallels/253proj/CMPE253/controllers/controller.py"
+controller1="/home/parallels/253proj/CMPE253/controllers/controller.py"
+controller2="/home/parallels/253proj/CMPE253/controllers/controller.py"
+controller3="/home/parallels/253proj/CMPE253/controllers/controller.py"
+
+ryu_path=/home/parallels/ryu/bin/ryu-manager
+jar_path=/home/parallels/253proj/CMPE253/controllers
+dir=/home/parallels/253proj/CMPE253
+
+observe=1
+rest=1
+
+usage(){
+	echo "usage: run-controller.h [[-f file]] [[-o]] [[-r]]"
+}
+
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-f | --file ) shift
+			      controller=$1
+			      ;;
+		-o | --observe-links )
+				       observe=0
+				       ;;
+		-r | --rest ) 
+			      rest=0
+			      ;;
+		* )	      usage
+			      exit 1
+	esac
+	shift
+done
+#Starting jar files
+echo "STARTING BFT SMART SERVERS"
+java -jar $jar_path/sdnstore.jar -server 0  /home/parallels/253proj/CMPE253/controllers/c0/nib.txt &
+sleep 2
+java -jar $jar_path/sdnstore.jar -server 1  /home/parallels/253proj/CMPE253/controllers/c1/nib.txt &
+sleep 2
+java -jar $jar_path/sdnstore.jar -server 2  /home/parallels/253proj/CMPE253/controllers/c2/nib.txt &
+sleep 2
+java -jar $jar_path/sdnstore.jar -server 3  /home/parallels/253proj/CMPE253/controllers/c3/nib.txt &
+sleep 2
+java -jar $jar_path/py4jclient.jar 4 9500 &
+sleep 2
+java -jar $jar_path/py4jclient.jar 5 9502 &
+sleep 2
+java -jar $jar_path/py4jclient.jar 6 9504 &
+sleep 2
+java -jar $jar_path/py4jclient.jar 7 9506 &
+sleep 2
+
+
+
+if [ "$observe" == "1"  ] && [ "$rest" == "1"  ]; then
+	sudo $ryu_path $controller0 ryu.app.ofctl_rest --observe-links --config-file $dir/controllers/c0/param.conf &
+	sleep 2
+	sudo $ryu_path $controller1 ryu.app.ofctl_rest --observe-link --ofp-tcp-listen-port 6634 --config-file $dir/controllers/c1/param.conf &
+	sleep 2
+	sudo $ryu_path $controller2 ryu.app.ofctl_rest --observe-link --ofp-tcp-listen-port 6635 --config-file $dir/controllers/c2/param.conf &
+	sleep 2
+	sudo $ryu_path $controller3 ryu.app.ofctl_rest --observe-link --ofp-tcp-listen-port 6636 --config-file $dir/controllers/c3/param.conf &
+elif [ "$observe" == "1" ]; then
+	sudo $ryu_path $controller0 --observe-links --config-file $dir/controllers/c0/param.conf &
+	sleep 2
+	sudo $ryu_path $controller1 --observe-links --ofp-tcp-listen-port 6634 --config-file $dir/controllers/c1/param.conf &
+	sleep 2
+	sudo $ryu_path $controller2 --observe-links --ofp-tcp-listen-port 6635 --config-file $dir/controllers/c2/param.conf &
+	sleep 2
+	sudo $ryu_path $controller3 --observe-links --ofp-tcp-listen-port 6636 --config-file $dir/controllers/c3/param.conf &
+elif [ "$rest" == "1" ]; then
+	sudo $ryu_path $controller0 ryu.ap.ofctl_rest --config-file $dir/controllers/c0/param.conf &
+	sudo $ryu_path $controller1 ryu.ap.ofctl_rest --ofp-tcp-listen-port 6634 --config-file $dir/controllers/c1/param.conf &
+	sudo $ryu_path $controller2 ryu.ap.ofctl_rest --ofp-tcp-listen-port 6635 --config-file $dir/controllers/c2/param.conf &
+	sudo $ryu_path $controller3 ryu.ap.ofctl_rest --ofp-tcp-listen-port 6636 --config-file $dir/controllers/c3/param.conf &
+else
+	sudo $ryu_path $controller0 --config-file $dir/controllers/c0/param.conf &
+	sudo $ryu_path $controller1 --ofp-tcp-listen-port 6634 --config-file $dir/controllers/c1/param.conf &
+	sudo $ryu_path $controller2 --ofp-tcp-listen-port 6635 --config-file $dir/controllers/c2/param.conf &
+    sudo $ryu_path $controller3 --ofp-tcp-listen-port 6636 --config-file $dir/controllers/c3/param.conf &
+fi
